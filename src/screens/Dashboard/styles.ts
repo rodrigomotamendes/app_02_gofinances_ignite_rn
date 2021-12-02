7d1c246a8df5994deb9496733eeca43860316fb1
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2021-12-02 16:28:40 -0300

    first commit

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-Content: center;
+  align-Items: center;
+
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: 24px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.title}
+`;
