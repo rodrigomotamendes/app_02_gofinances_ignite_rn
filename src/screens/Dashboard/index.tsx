868950715e0eaf5eeade7aa6008a443e73088ce3
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-01-04 01:50:33 -0300

    Finalizando a listagem de transações

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import { HighlightCard } from '../../components/HighlightCard';
-import { TransactionCard } from '../../components/TransactionCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
   Container,
@@ -20,34 +20,44 @@ import {
   TransactionList
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard(){
-  const data = [
-    {
+  const data: DataListProps[] = [
+  {
+    id: '1',
+    type: 'positive',
     title:"Desenvolvimento de site",
     amount:"R$ 12.000,00",
     category: {
       name:'Vendas',
       icon:'dollar-sign',
     },
-    date:"13/04/20"
+    date:"13/04/2020"
   },
   {
-    title:"Desenvolvimento de site",
-    amount:"R$ 12.000,00",
+    id: '2',
+    type: 'negative',
+    title:"Hamburgeria Pizzy",
+    amount:"R$ 59,00",
     category: {
-      name:'Vendas',
-      icon:'dollar-sign',
+      name:'Alimentação',
+      icon:'coffee',
     },
-    date:"13/04/20"
+    date:"10/04/2020"
   },
   {
-    title:"Desenvolvimento de site",
-    amount:"R$ 12.000,00",
+    id: '3',
+    type: 'negative',
+    title:"Aluguel do apartamento",
+    amount:"R$ 1.200,00",
     category: {
-      name:'Vendas',
-      icon:'dollar-sign',
+      name:'Casa',
+      icon:'shopping-bag',
     },
-    date:"13/04/20"
+    date:"27/03/2020"
   },
 ];
 
@@ -92,11 +102,8 @@ export function Dashboard(){
         <Title>Listagem</Title>
         <TransactionList 
           data={data}
+          keyExtractor={item => item.id}
           renderItem={({ item }) => <TransactionCard data={item} />}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            paddingBottom: getBottomSpace()
-          }}
         />
       </Transactions>
     </Container>
