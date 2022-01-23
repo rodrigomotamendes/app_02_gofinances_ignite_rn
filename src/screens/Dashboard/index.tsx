58e6871ef301c2db62d4b97578fdb49b4883ed6b
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-01-23 17:26:37 -0300

    RectButton e BorderlessButton

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -17,7 +17,8 @@ import {
   HighlightCards,
   Transactions,
   Title,
-  TransactionList
+  TransactionList,
+  LogoutButton,
 } from './styles'
 
 export interface DataListProps extends TransactionCardProps {
@@ -73,7 +74,9 @@ export function Dashboard(){
             </User>
           </UserInfo>
           
-          <Icon name="power"/>
+          <LogoutButton onPress={() => {}}>
+            <Icon name="power"/>
+          </LogoutButton>
         </UserWrapper>
       </Header>
 
