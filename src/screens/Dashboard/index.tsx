5423047b38cd8af5588139da7ba560b27cf35782
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-01-31 18:10:53 -0300

    Finalizando a lista

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { useFocusEffect } from '@react-navigation/native';
 
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
@@ -59,12 +61,17 @@ export function Dashboard(){
     });
 
     setData(transactionsFormatted);
+    console.log(transactionsFormatted);
 
   }
 
   useEffect(() => {
     loadTransactions();
   }, [])
+
+  useFocusEffect(useCallback(() => {
+    loadTransactions();
+  }, []));
 
   return(
     <Container>
