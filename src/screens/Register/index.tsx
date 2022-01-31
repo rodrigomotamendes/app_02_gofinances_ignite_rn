751964c1cbb4c2a520cd8cf689e215d3b1b09d70
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-01-31 12:39:12 -0300

    Armazenando e recuperando dados

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   Modal, 
   TouchableWithoutFeedback, 
@@ -7,10 +7,10 @@ import {
 } from 'react-native';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { useForm } from 'react-hook-form';
 
-import { Input } from '../../components/Form/Input';
 import { InputForm } from '../../components/Form/InputForm';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
@@ -50,6 +50,8 @@ const schema = Yup.object().shape({
 export function Register(){
   const [transactionType, setTransactionType] = useState('');
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+  const dataKey = '@gofinances:transactions';
   
   const [category, setCategory] = useState({
     key: 'category',
@@ -76,7 +78,7 @@ export function Register(){
     setCategoryModalOpen(false);
   }
 
-  function handleRegister(form : FormData){
+  async function handleRegister(form : FormData){
     if(!transactionType)
       return Alert.alert ('Selecione o tipo da transação');
 
@@ -90,8 +92,25 @@ export function Register(){
       category: category.key
     }
 
-    console.log(data)
+    try {
+      await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Não foi possível salvar');
+    }
+
   }
+
+  useEffect(() => {
+    async function loadData(){
+      const data = await AsyncStorage.getItem(dataKey);
+      console.log(JSON.parse(data!));
+    }
+
+    loadData();
+
+  }, []);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
