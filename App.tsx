999d09c26cad5d18cd182e7c2c32cb9571e423a4
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-02-04 18:49:07 -0300

    Criando nosso próprio Hook

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 import { SignIn } from './src/screens/SignIn';
-import { AuthContext } from './src/AuthContext';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -39,9 +39,9 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        <AuthContext.Provider value={[]}>
+        <AuthProvider>
           <SignIn />
-        </AuthContext.Provider>
+        </AuthProvider>
 
       </NavigationContainer>
     </ThemeProvider>
---
+++ src/hooks/auth.tsx
@@ -0,0 +1,45 @@
+import React, { 
+  createContext, 
+  ReactNode,
+  useContext,
+} from 'react';
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface AuthContextData {
+  user: User;
+}
+
+const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps){
+
+  const user = {
+    id: '232353525',
+    name: 'Rodrigo Mendes',
+    email: 'rodrigo@email.com',
+  };
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth(){
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth }
