221253a81a0fc35424557778ee9a75942209a394
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-02-07 14:22:41 -0300

    Conectando com uma conta Google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,7 +2,10 @@ import React, {
   createContext, 
   ReactNode,
   useContext,
+  useState,
 } from 'react';
+
+import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -15,22 +18,57 @@ interface User {
   photo?: string;
 }
 
-interface AuthContextData {
+interface IAuthContextData {
   user: User;
+  signInWithGoogle(): Promise<void>;
 }
 
-const AuthContext = createContext({} as AuthContextData);
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  },
+  type: string;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps){
+  const [user, setUser] = useState<User>({} as User);
 
-  const user = {
-    id: '232353525',
-    name: 'Rodrigo Mendes',
-    email: 'rodrigo@email.com',
-  };
+  async function signInWithGoogle(){
+    try {
+      const CLIENT_ID = '308224555688-4rf4ltcpj223l77rtjdbhc2k4ldflbkf.apps.googleusercontent.com';
+      const REDIRECT_URI = 'https://auth.expo.io/@rodrigomendes/gofinances';
+      const RESPONSE_TYPE = 'token';
+      const SCOPE = encodeURI('profile email');
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const { type, params } = await AuthSession
+        .startAsync({ authUrl }) as AuthorizationResponse;
+
+      if (type === 'success') {
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        const userInfo = await response.json();
+
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture,
+        });
+      }
+
+    } catch (error) {
+      // throw new Error(error);
+    }
+  }
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ 
+        user, 
+        signInWithGoogle 
+      }}>
       { children }
     </AuthContext.Provider>
   )
