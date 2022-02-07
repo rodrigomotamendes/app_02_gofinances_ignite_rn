4149600bbb0e6a3de20d89de324d50ef3be84dcf
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-02-07 16:53:08 -0300

    Conectando com uma Conta Apple

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -21,7 +21,7 @@ import {
 } from './styles';
 
 export function SignIn(){
-  const { signInWithGoogle } = useAuth();
+  const { signInWithGoogle, signInWithApple } = useAuth();
 
   async function handleSignInWithGoogle(){
     try {
@@ -30,7 +30,15 @@ export function SignIn(){
       console.log(error);
       Alert.alert('Não foi possível conectar a conta Google')
     }
-    
+  }
+
+  async function handleSignInWithApple(){
+    try {
+      await signInWithApple();
+    } catch(error) {
+      console.log(error);
+      Alert.alert('Não foi possível conectar a conta Apple')
+    }
   }
 
   return(
@@ -66,6 +74,7 @@ export function SignIn(){
           <SignInSocialButton 
             title='Entrar com Apple'
             svg={AppleSvg}
+            onPress={handleSignInWithApple}
           />
 
         </FooterWrapper>
