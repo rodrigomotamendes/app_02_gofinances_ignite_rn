8b74d8a048214374300eced3a28b6ca001953b8d
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-05-19 17:26:10 -0300

    Testes com Contexto

--- src/components/Form/Input/index.spec.tsx
+++ src/components/Form/Input/index.spec.tsx
@@ -2,21 +2,37 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 
 import { Input } from '.';
+import { ThemeProvider } from 'styled-components/native';
+import theme from '../../../global/styles/theme';
+
+const Providers: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    { children }
+  </ThemeProvider>
+);
 
 describe('Input Component', () => {
   it('must specific have border when active', () => {
-    const { getByTestId } = render(
+    const { getByTestId, debug } = render(
       <Input 
         testID='input-email'
         placeholder='E-mail'
         keyboardType='email-address'
         autoCorrect={false}
         active={true}
-      />
+      />,
+      {
+        wrapper: Providers
+      }
     );
+
+    debug();
 
     const inputComponent = getByTestId('input-email');
     expect(inputComponent.props.style[0].borderColor)
-      .toEqual('#e83f5b');
+      .toEqual(theme.colors.attention);
+    
+    expect(inputComponent.props.style[0].borderWidth)
+      .toEqual(3);
   })
 });
