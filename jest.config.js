0d44a6632c9bb10bdecf9b08c55a4c13a32ee5dd
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-05-20 01:50:49 -0300

    Cobertura de Código

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,13 @@ module.exports = {
     "/node_modules",
     "/android",
     "/ios"
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx"
+  ],
+  coverageReporters: [
+    "lcov"
   ]
 }
